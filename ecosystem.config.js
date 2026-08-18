c3520c07780ceb71d94087cd0eafafad82042f1e
root <root@vmi3490888.contaboserver.net>
2026-08-18 17:56:53 +0530

    Fix PATH variable and script binary in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,32 @@
+const MONOREPO = '/home/f2hfresh/htdocs/f2hfresh.com';
+
 module.exports = {
   apps: [
     {
       name: 'api-f2hfresh',
-      cwd: '/home/f2hfresh/htdocs/f2hfresh.com/apps/api',
-      script: 'dist/main.js',
+      cwd: `${MONOREPO}/apps/api`,
+      script: 'dist/src/main.js',
       env: {
         NODE_ENV: 'production',
         APP_PORT: 5001,
         PORT: 5001,
+        PATH: `${MONOREPO}/node_modules/.bin:${process.env.PATH}`,
       },
+      out_file: '/home/f2hfresh/logs/pm2/api-out.log',
+      error_file: '/home/f2hfresh/logs/pm2/api-err.log',
     },
     {
       name: 'frontend-f2hfresh',
-      cwd: '/home/f2hfresh/htdocs/f2hfresh.com/apps/web',
-      script: 'npm',
-      args: 'start',
+      cwd: `${MONOREPO}/apps/web`,
+      script: `${MONOREPO}/node_modules/.bin/next`,
+      args: 'dev -H 0.0.0.0 -p 5002',
       env: {
         NODE_ENV: 'production',
         PORT: 5002,
+        PATH: `${MONOREPO}/node_modules/.bin:${process.env.PATH}`,
       },
+      out_file: '/home/f2hfresh/logs/pm2/frontend-out.log',
+      error_file: '/home/f2hfresh/logs/pm2/frontend-err.log',
     },
   ],
 };
